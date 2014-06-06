a10f6766db5bf797ae6125d3d088f5d328830b41
infoburp <gwolfendale@gmail.com>
2014-06-06 01:58:55 +0100

    fixed holiday feature for users with noscript activated

--- index.js
+++ index.js
@@ -9,35 +9,29 @@ var month = d.getMonth();
 //1-12 instead of 0-11 months
 month += 1;
 
-holidaydiv.innerHTML = lang;
 //display appropriate text for today
-holidaydiv.innerHTML = 'Today is ';
 if (day == 25 && month == 12)
 {
-	holidaydiv.innerHTML += 'Christmas day.';
+	holidaydiv.innerHTML = 'Today is Christmas day.';
 }
 if (day == 1 && month == 1)
 {
-	holidaydiv.innerHTML += 'New Years day.';
+	holidaydiv.innerHTML = 'Today is New Years day.';
 }
 if (day == 31 && month == 12)
 {
-	holidaydiv.innerHTML += 'New Years eve.';
+	holidaydiv.innerHTML = 'Today is New Years eve.';
 }
 if (day == 17 && month == 3)
 {
-	holidaydiv.innerHTML += 'St. Patricks day.';
+	holidaydiv.innerHTML = 'Today is St. Patricks day.';
 }
 if (day == 26 && month == 12)
 {
-	holidaydiv.innerHTML += 'Boxing day.';
+	holidaydiv.innerHTML = 'Today is Boxing day.';
 }
 if (day == 24 && month == 12)
 {
-	holidaydiv.innerHTML += 'Christmas eve.';
+	holidaydiv.innerHTML = 'Today is Christmas eve.';
 }
 
-if (holidaydiv.innerHTML = 'Today is ') 
-{
-	holidaydiv.innerHTML += 'not a holiday, back to work!'
-}
